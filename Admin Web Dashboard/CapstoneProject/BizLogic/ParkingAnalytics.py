5dc2c2b8f5cad97c7284cca3514e09d79df1a3f4
abhi-jeet589 <abhijeetkarmakar23@gmail.com>
2024-03-23 03:18:16 -0600

    Added backend python django server and batch data pipelines for web admin dashboard component

---
+++ Admin Web Dashboard/CapstoneProject/BizLogic/ParkingAnalytics.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import logging
+
+def parkingDetails(parkingDict):
+    logger = logging.getLogger(__name__)
+    logger.info("Aggregating data")
+    parkingLotDetails = {}
+    columnIgnoreList = {'Gate','Settings','Status'}
+    for parkingLotName in parkingDict.keys():
+        totalCoveredSlots, totalOccupiedSlots, totalVipSlots = 0,0,0
+        df_parkinglot = pd.DataFrame(parkingDict[parkingLotName])
+        df_parkinglot = df_parkinglot.drop(columns=columnIgnoreList,errors='ignore')
+        coveredSlots = df_parkinglot.loc['covered'].aggregate(func="sum",axis=0)
+        occupiedSlots = df_parkinglot.loc['occupied'].aggregate(func="sum",axis=0)
+        vipSlots = df_parkinglot.loc['vip'].aggregate(func="sum",axis=0)
+        totalCoveredSlots += coveredSlots
+        totalOccupiedSlots += occupiedSlots
+        totalVipSlots += vipSlots
+        for col in columnIgnoreList:
+            parkingDict[parkingLotName].pop(col,None)
+        parkingLotDetails[parkingLotName] = {
+        'totalParkingSlots': len(df_parkinglot.columns),
+        'totalOccupiedSlots': totalOccupiedSlots,
+        'totalAvailableSlots': len(df_parkinglot.columns) - totalOccupiedSlots,
+        'pct_utilization': '{:.2f}'.format((totalOccupiedSlots/len(df_parkinglot.columns))*100),
+        'totalCoveredSlots': totalCoveredSlots,
+        'totalVipSlots' : totalVipSlots,
+        'parkingSlotDetails': parkingDict[parkingLotName]
+    }
+        del df_parkinglot
+        
+    return parkingLotDetails
